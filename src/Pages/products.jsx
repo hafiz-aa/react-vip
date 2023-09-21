09ab83db2e6156d52b3d8b0a3a64ee356c1de34c
hafiz-aa <mhafiz.aa@gmail.com>
2023-09-21 16:35:47 +0700

    Implementasi React Redux

--- src/Pages/products.jsx
+++ src/Pages/products.jsx
@@ -1,8 +1,9 @@
-import { Fragment, useState, useEffect, useRef } from "react";
+import { Fragment, useState, useEffect } from "react";
 import CardProduct from "../components/Fragments/CardProducts";
-import Button from "../components/Elements/Button";
 import { getProducts } from "../services/product.service";
 import { useLogin } from "../hooks/useLogin";
+import TableCart from "../components/Fragments/TableCart";
+import Navbar from "../components/Layouts/Navbar";
 // import Counter from "../components/Fragments/Counter";
 
 /**
@@ -10,14 +11,8 @@ import { useLogin } from "../hooks/useLogin";
  */
 
 const ProductsPage = () => {
-  const [cart, setCart] = useState([]);
-  const [totalPrice, setTotalPrice] = useState(0);
   const [products, setProducts] = useState([]);
-  const username = useLogin();
-
-  useEffect(() => {
-    setCart(JSON.parse(localStorage.getItem("cart")) || []);
-  }, []);
+  useLogin();
 
   useEffect(() => {
     getProducts((data) => {
@@ -25,48 +20,9 @@ const ProductsPage = () => {
     });
   }, []);
 
-  useEffect(() => {
-    if (products.length > 0 && cart.length > 0) {
-      const sum = cart.reduce((acc, item) => {
-        const product = products.find((product) => product.id === item.id);
-        return acc + product.price * item.qty;
-      }, 0);
-      setTotalPrice(sum);
-      localStorage.setItem("cart", JSON.stringify(cart));
-    }
-  }, [products, cart]);
-
-  const handleLogout = () => {
-    localStorage.removeItem("token");
-    localStorage.removeItem("password");
-    window.location.href = "/login";
-  };
-
-  const handleAddToCart = (id) => {
-    if (cart.find((item) => item.id === id)) {
-      setCart(cart.map((item) => (item.id === id ? { ...item, qty: item.qty + 1 } : item)));
-    } else {
-      setCart([...cart, { id, qty: 1 }]);
-    }
-  };
-
-  const totalPriceRef = useRef(null);
-  useEffect(() => {
-    if (cart.length > 0) {
-      totalPriceRef.current.style.display = "table-row";
-    } else {
-      totalPriceRef.current.style.display = "none";
-    }
-  }, [cart, products]);
-
   return (
     <Fragment>
-      <div className="flex justify-end h-20 bg-blue-600 text-white items-center px-10">
-        {username}
-        <Button className="ml-5 bg-black" onClick={handleLogout}>
-          Logout
-        </Button>
-      </div>
+      <Navbar />
       <div className="flex justify-center py-5">
         <div className="w-4/6 flex flex-wrap">
           {products.length > 0 &&
@@ -74,44 +30,13 @@ const ProductsPage = () => {
               <CardProduct key={product.id}>
                 <CardProduct.Header image={product.image} id={product.id} />
                 <CardProduct.Body name={product.title}>{product.description}</CardProduct.Body>
-                <CardProduct.Footer price={product.price} id={product.id} handleAddToCart={handleAddToCart} />
+                <CardProduct.Footer price={product.price} id={product.id} />
               </CardProduct>
             ))}
         </div>
         <div className="w-1/4">
-          <h1 className="text-3xl font-bold text-blue-600">Cart</h1>
-          <table className="text-left table-auto border-separate border-spacing-x-5">
-            <thead>
-              <tr>
-                <th>Product</th>
-                <th>Price</th>
-                <th>Qty</th>
-                <th>Total</th>
-              </tr>
-            </thead>
-            <tbody>
-              {products.length > 0 &&
-                cart.map((item) => {
-                  const product = products.find((product) => product.id === item.id);
-                  return (
-                    <tr key={item.id}>
-                      <td>{product.title.substring(0, 20)}...</td>
-                      <td>$ {product.price.toLocaleString("id-ID")}</td>
-                      <td>{item.qty}</td>
-                      <td>$ {(product.price * item.qty).toLocaleString("id-ID")}</td>
-                    </tr>
-                  );
-                })}
-              <tr ref={totalPriceRef}>
-                <td colSpan={3}>
-                  <b>Total Price</b>
-                </td>
-                <td>
-                  <b>$ {totalPrice.toLocaleString("id-ID")}</b>
-                </td>
-              </tr>
-            </tbody>
-          </table>
+          <h1 className="text-3xl font-bold text-blue-600 ml-5 mb-2">Cart</h1>
+          <TableCart products={products} />
         </div>
       </div>
       {/* <div className="mt-5 flex justify-center">
--- src/main.jsx
+++ src/main.jsx
@@ -8,6 +8,8 @@ import ErrorPage from "./Pages/404.jsx";
 import ProductsPage from "./Pages/products";
 import ProfilePage from "./Pages/profile";
 import DetailProductPage from "./Pages/detailProduct";
+import { Provider } from "react-redux";
+import store from "./redux/store";
 
 const router = createBrowserRouter([
   {
@@ -48,6 +50,8 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
   </React.StrictMode>
 );
